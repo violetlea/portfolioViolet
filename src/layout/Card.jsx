d8805670a0c93f6b1eb518765f47cd109dcf59ac
violetic <tuanalianabila.aziz@gmail.com>
2025-11-01 15:53:29 +0800

    add styling to about page

--- src/layout/Card.jsx
+++ src/layout/Card.jsx
@@ -13,33 +13,74 @@ export default function Card() {
 						className="bgCard h-100 w-80 md:h-110 md:w-90 
                         rounded-xl ">
 						<div className="relative">
-							<img src="src/images/picture for portfolio.png" />
-								<div>
-									<div
-										className="absolute top-2 bg-gray-300  
+							<img
+								src="src/images/picture for portfolio.png"
+								className="h-100 w-80 md:h-110 md:w-90"
+							/>
+							<div>
+								<div
+									className="absolute top-2 bg-gray-300  
                                         left-1  p-0.5 rounded-sm drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.5)]
                                         ">
-										<p className="font-bold text-gray-500 drop-shadow-sky-900
+									<p
+										className="font-bold text-gray-500 drop-shadow-sky-900
                                              text-xs italic">
-											SPECIAL
+										SPECIAL
+									</p>
+								</div>
+								<div className="absolute top-1 right-40 md:right-45 ">
+									<h1 className="text-xl md:text-2xl font-bold textStroke">
+										AliaNabila
+									</h1>
+								</div>
+								<div className="absolute top-2 md:top-2.5 right-18.5 md:right-20 ">
+									<h1 className="text-2xs font-bold textStroke">HP</h1>
+								</div>
+								<div className="absolute top-1 right-10">
+									<h1 className="text-xl md:text-2xl font-bold textStroke">
+										999
+									</h1>
+								</div>
+								<div className="absolute top-1.5 md:top-2 right-3">
+									<img src="src/images/paw (1).png" className="h-6" />
+								</div>
+								<div className="absolute bottom-35 left-1">
+									<div
+										className="bg-linear-to-br from-slate-900 via-red-600 to-slate-900 pt-0.2 pb-0.2 pr-3 pl-3 
+											border-2 border-gray-400 rounded-sm inset-shadow-red-50">
+										<p className="text-xs text-white font-semibold tracking-widest italic">
+											Ability
 										</p>
 									</div>
-                                    <div className="absolute top-1 right-40 md:right-45 ">
-                                        <h1 className="text-xl md:text-2xl font-bold textStroke">AliaNabila</h1>
-                                    </div>
-									<div className="absolute top-2 md:top-2.5 right-18.5 md:right-20 ">
-                                        <h1 className="text-2xs font-bold textStroke">HP</h1>
-                                    </div>
-									<div className="absolute top-1 right-10">
-                                        <h1 className="text-xl md:text-2xl font-bold textStroke">999</h1>
-                                    </div>
-									<div className="absolute top-1.5 md:top-2 right-3">
-										<img src="src/images/paw (1).png" className="h-6"/>
-
-									</div>
-
 								</div>
-
+								<div className="absolute bottom-34 left-22">
+									<h1 className="text-xl  font-extrabold text-red-500 textStroke">
+										Eagle Eye
+									</h1>
+								</div>
+								<div className="absolute bottom-25 left-1">
+									<h2 className="text-sm textStroke font-bold">
+										She can spot a cat within a seconds and steal opponent's
+										cat!
+									</h2>
+								</div>
+								<div className="absolute inline-flex bottom-15 left-1">
+									<img
+										src="src/images/paw (1).png"
+										className="h-6 drop-shadow-black "
+									/>
+									<img
+										src="src/images/paw (1).png"
+										className="h-6 drop-shadow-black"
+									/>
+								</div>
+								<div className="absolute bottom-15 left-22">
+									<h1 className="font-bold text-lg textStroke">Wink & Meow</h1>
+								</div>
+								<div className="absolute bottom-15 right-3">
+									<h1 className="font-bold text-lg textStroke">120</h1>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,8 +4,87 @@ export default function AboutMe() {
 	return (
 		<>
 			<BoxContainer>
-				<h1>About Me</h1>
-				<p>Name : {aboutMe.basicInfo.name}</p>
+				<div className="text-center">
+					<div className="inline-flex gap-2">
+						<img
+							src="src/images/pngimg.com - pokeball_PNG24.png"
+							className="h-6"
+						/>
+						<h1 className="font-bold text-xl">About Me</h1>
+						<img
+							src="src/images/pngimg.com - pokeball_PNG24.png"
+							className="h-6"
+						/>
+					</div>
+					<div className="grid grid-cols-1 gap-2 justify-items-center p-2">
+						<div className="bg-violet-300 rounded-full p-5 border border-white shadow-xl">
+							<img src="src/images/faceannoy.png" className="h-20" />
+						</div>
+					</div>
+					<div className="grid grid-cols-1 md:grid-cols-2 gap-2 justify-items-center p-2">
+						<div className="border rounded-full p-2 w-full">
+							<p>Name : {aboutMe.basicInfo.name}</p>
+						</div>
+						<div className="border rounded-full p-2 w-full">
+							<p>Date of birth : {aboutMe.basicInfo.DOB}</p>
+						</div>
+						<div className="border rounded-full p-2 w-full">
+							<p>Email : {aboutMe.basicInfo.mail}</p>
+						</div>
+						<div className="border rounded-full p-2 w-full">
+							<p>Email : {aboutMe.basicInfo.location}</p>
+						</div>
+					</div>
+					<div className="p-4 w-full">
+						<p>Description :</p>
+						<p className="text-sm md:text-base italic">
+							{aboutMe.basicInfo.longText}
+						</p>
+					</div>
+					<div className="grid grid-cols-2 gap-2 p-2 mt-2 ">
+						<div className="p-2">
+							<p className="italic font-bold">I can speak...</p>
+							<ul>
+								<li>{aboutMe.Languages.lang1}</li>
+								<li>{aboutMe.Languages.lang2}</li>
+								<li>{aboutMe.Languages.lang3}</li>
+								<li>{aboutMe.Languages.lang4}</li>
+							</ul>
+						</div>
+						<div className="p-2">
+							<p className="italic font-bold">I love/enjoy...</p>
+							<ul>
+								<li>{aboutMe.Interest.int1}</li>
+								<li>{aboutMe.Interest.int2}</li>
+								<li>{aboutMe.Interest.int3}</li>
+								<li>{aboutMe.Interest.int4}</li>
+								<li>{aboutMe.Interest.int5}</li>
+							</ul>
+						</div>
+						{/* socials */}
+					</div>
+					<div className="grid grid-cols-1 justify-items-center  p-2 w-full">
+						<div>
+							<div className="font-bold italic">
+								<p className="pb-3">Connect with me at...</p>
+								<div className="inline-flex gap-3 ">
+									<a
+										href={aboutMe.Links.github}
+										target="_blank"
+										className="cursor-pointer">
+										<img src="src/images/github.png" className="h-10" />
+									</a>
+									<a
+										href={aboutMe.Links.linkedin}
+										target="_blank"
+										className="cursor-pointer">
+										<img src="src/images/linkedin.png" className="h-10" />
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</BoxContainer>
 		</>
 	);
